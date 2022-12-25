3660c96256dafb166636f9e10a166375ba4bed3e
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2022-12-25 13:29:53 +0530

    #include<stdio.h> int main() {     int f=0,sec=1,n,next=0;     scanf("%d",&n);     while(n>next)     {         next=f+sec;         f=sec;         sec=next;     }     if(n-f<sec-n)     {         printf("%d",f);     }     else if(n-f==sec-n)     {         printf("%d %d",f,sec);     }     else     {         printf("%d",sec);     } }

---
+++ nearest_fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int f=0,sec=1,n,next=0;
+    scanf("%d",&n);
+    while(n>next)
+    {
+        next=f+sec;
+        f=sec;
+        sec=next;
+    }
+    if(n-f<sec-n)
+    {
+        printf("%d",f);
+    }
+    else if(n-f==sec-n)
+    {
+        printf("%d %d",f,sec);
+    }
+    else
+    {
+        printf("%d",sec);
+    }
+}
