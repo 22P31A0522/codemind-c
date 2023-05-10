443d755466c7639786cb750d22b87ebb6c089a2e
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2023-05-10 16:38:55 +0530

    #include<stdio.h> int main() {     int a,b,c,i;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         c=a*i;         if(c%b==0)         {             printf("%d",c);             break;         }     }
    }

--- LCM.c
+++ LCM.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2,max;
-    scanf("%d%d",&n1,&n2);
-    max=(n1>n2) ? n1:n2;
-    while(1)
+    int a,b,c,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
     {
-        if((max%n1==0)&&(max%n2==0))
+        c=a*i;
+        if(c%b==0)
         {
-            printf("%d",max);
+            printf("%d",c);
             break;
         }
-        ++max;
     }
+    
 }
